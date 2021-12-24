14fc69e472ccbe42936453a72ff3fc46949b10c1
Aleksandr <menshikov.a.v@icloud.com>
2021-12-24 14:05:41 +0300

    Fix urls

--- santa_secret/urls.py
+++ santa_secret/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
 
@@ -28,4 +27,4 @@ urlpatterns = [
 if settings.DEBUG and settings.MEDIA_ROOT:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

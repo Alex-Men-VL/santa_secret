7ede6820a6ae49a305bd82bbcf0c405b634f1215
Aleksandr <menshikov.a.v@icloud.com>
2021-12-25 22:50:19 +0300

    Change the animation of the message when copying

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+let popup = document.getElementById('mypopup');
+
 document.body.onclick = (event) => {
     const elem = event.target;
     if (elem.classList.contains('form-control')) {
         navigator.clipboard.writeText(elem.value)
-        successMessage.classList.add('active')
-        setTimeout(() => successMessage.classList.remove('active'), 2000)
+        popup.style.display='block'
+        setTimeout(() => popup.style.display='none', 1500)
     }
 }
